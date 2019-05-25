8d11fa34e30a719774a675177e516857c4349f40
Dayanand Deepak <dayananddeepak7@gmail.com>
2019-05-25 18:53:29 +0530

    Angular Pipe
    Add new Feature Angular Pipe

--- Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -8,19 +8,19 @@ export class EmployeeListComponent {
     employees : any[];
      constructor() {
     this.employees= [
-        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dataOfBirth: '25/6/1989'},
-        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dataOfBirth: '11/7/1988'},
-        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dataOfBirth: '29/5/1990'},
-        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dataOfBirth: '19/9/1996'},
+        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dataOfBirth: '9/25/1989'},
+        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dataOfBirth: '11/17/1988'},
+        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dataOfBirth: '12/15/1990'},
+        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dataOfBirth: '10/29/1996'},
     ];
   }
   getEmployees():void {
       this.employees=[
-        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dataOfBirth: '25/6/1989'},
-        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dataOfBirth: '11/7/1988'},
-        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dataOfBirth: '29/5/1990'},
-        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dataOfBirth: '19/9/1996'},
-        { code : 'emp105', name: 'Hary', gender :'Female', annualSalary : 8900, dataOfBirth: '17/5/1994'},
+        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dataOfBirth: '9/25/1989'},
+        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dataOfBirth: '11/17/1988'},
+        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dataOfBirth: '12/15/1990'},
+        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dataOfBirth: '10/28/1996'},
+        { code : 'emp105', name: 'Hary', gender :'Female', annualSalary : 8900, dataOfBirth: '10/5/1994'},
       ];
   }
   trackByEmpCode(index: number, employee: any): string {
