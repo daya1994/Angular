44eb8cb685b3f7add2bc4a4e647e8edbba6b27c6
Dayanand Deepak <dayananddeepak7@gmail.com>
2019-06-01 21:06:37 +0530

    Nested component and Component
    Upadated the code of Component and Nested Component

--- Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -1,21 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IEmployee} from './employee';
 @Component ({
      selector: 'list-employee',
      templateUrl: 'employeeList.component.html',
       styleUrls : ['employeeList.component.css']
 })
 export class EmployeeListComponent {
-    employees : any[];
+    employees : IEmployee[];
 
     selectedEmployeeCountRadioButton : string = 'All';
      constructor() {
     this.employees= [
-        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dataOfBirth: '6/9/1989'},
-        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dataOfBirth: '11/11/1988'},
-        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dataOfBirth: '12/8/1990'},
-        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dataOfBirth: '10/6/1996'},
-        { code : 'emp105', name: 'Nary', gender :'Female', annualSalary : 8900, dataOfBirth: '9/6/1993'},
-        { code : 'emp105', name: 'Hary', gender :'Male', annualSalary : 8900, dataOfBirth: '6/9/1992'},
+        { code : 'emp101', name: 'Tom', gender :'Male', annualSalary : 5500, dateOfBirth: '6/9/1989'},
+        { code : 'emp102', name: 'Staurt', gender :'Male', annualSalary : 6500, dateOfBirth: '11/11/1988'},
+        { code : 'emp103', name: 'Mike', gender :'Male', annualSalary : 9500, dateOfBirth: '12/8/1990'},
+        { code : 'emp104', name: 'Mary', gender :'Female', annualSalary : 7500, dateOfBirth: '10/6/1996'},
+        { code : 'emp105', name: 'Nary', gender :'Female', annualSalary : 8900, dateOfBirth: '9/6/1993'},
+        { code : 'emp105', name: 'Hary', gender :'Male', annualSalary : 8900, dateOfBirth: '6/9/1992'},
       ];
   }
 
